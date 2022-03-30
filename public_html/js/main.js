73497119a84206ad0648ef65bbe83c9ba5f2272a
Luka <lukakeni95@gmail.com>
2022-03-30 20:39:46 +0200

    home page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -24,6 +24,25 @@ $(document).ready(function(){
     });
     animation();
 
+
+   // team-slider
+    
+    if($('.team-members-slider').length > 0){
+        $(".team-members-slider").owlCarousel({
+            
+            loop:true,
+            autoplay:true,
+            
+            responsive:{
+                0:{
+                    items:1,
+                    margin:0
+                }
+              
+            }
+            
+        });
+    }
     
     
     
